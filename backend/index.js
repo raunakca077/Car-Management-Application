af4c22521416633c10012b50af64d3c008e63659
Raunak chauhan <raunak.2022ca077@mnnit.ac.in>
2024-11-15 06:47:04 +0530

    remove nodemon command

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
-app.use('/',(req,res)=>{"welcome buddy"})
+app.use('/',(req,res)=>{res.send("welcome buddy")})
 app.use('/api/users', userRoutes);
 app.use('/api/cars', carRoutes);
 
