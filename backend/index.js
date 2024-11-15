3c971d701ae1a7389c921972899ef0521b761627
Raunak chauhan <raunak.2022ca077@mnnit.ac.in>
2024-11-15 05:56:49 +0530

    update cors

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173', // Fallback to localhost
+    origin: 'https://car-management-application-ve1f.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
